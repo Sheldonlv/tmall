d3bd39d91e9a84ccb34f620b0d423071de57ed8d
Sheldonlv <904886110@qq.com>
2019-08-14 23:30:28 +0800

    初始化项目

---
+++ src/test/java/com/sheldon/TmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheldon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
